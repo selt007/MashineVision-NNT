e18af6a387093814abcbf237352929cc8e11215f
Alexandr <all1992ex@gmail.com>
2018-08-30 13:51:18 +0400

    Первоначальная обработка изображений. Необходимо доработать алгоритм.

--- NNTSearchChar/MainForm.Designer.cs
+++ NNTSearchChar/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NNTSearchChar
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -38,9 +38,10 @@
             // picture1
             // 
             this.picture1.BackColor = System.Drawing.Color.White;
+            this.picture1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.picture1.Location = new System.Drawing.Point(13, 13);
             this.picture1.Name = "picture1";
-            this.picture1.Size = new System.Drawing.Size(386, 386);
+            this.picture1.Size = new System.Drawing.Size(192, 192);
             this.picture1.TabIndex = 0;
             this.picture1.TabStop = false;
             this.picture1.Paint += new System.Windows.Forms.PaintEventHandler(this.picture1_Paint);
@@ -49,34 +50,35 @@
             // 
             // button
             // 
-            this.button.Location = new System.Drawing.Point(405, 360);
+            this.button.Location = new System.Drawing.Point(211, 320);
             this.button.Name = "button";
-            this.button.Size = new System.Drawing.Size(135, 39);
+            this.button.Size = new System.Drawing.Size(302, 39);
             this.button.TabIndex = 1;
-            this.button.Text = "button1";
+            this.button.Text = "Тест";
             this.button.UseVisualStyleBackColor = true;
             this.button.Click += new System.EventHandler(this.button_Click);
             // 
             // picture2
             // 
-            this.picture2.Location = new System.Drawing.Point(405, 13);
+            this.picture2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picture2.Location = new System.Drawing.Point(211, 12);
             this.picture2.Name = "picture2";
-            this.picture2.Size = new System.Drawing.Size(135, 134);
+            this.picture2.Size = new System.Drawing.Size(302, 302);
             this.picture2.TabIndex = 2;
             this.picture2.TabStop = false;
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(552, 411);
+            this.ClientSize = new System.Drawing.Size(525, 369);
             this.Controls.Add(this.picture2);
             this.Controls.Add(this.button);
             this.Controls.Add(this.picture1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form Image";
             ((System.ComponentModel.ISupportInitialize)(this.picture1)).EndInit();
---
+++ NNTSearchChar/MashineVision.cs
@@ -0,0 +1,89 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NNTSearchChar
+{
+    class MashineVision
+    {
+        //private Capture capture = new Capture();
+        public Bitmap imgOut;
+
+        public void ImageOut()
+        {
+            HaarCascade faceCascade = new HaarCascade("haarcascade_frontalface_alt.xml");
+            Image<Bgr, byte> image = new Image<Bgr, byte>(new Bitmap("photo.jpg"));//capture.QueryFrame();
+            Image<Gray, byte> grayImage = image.Convert<Gray, byte>();
+
+            var Face = faceCascade.Detect(grayImage);
+            foreach (var face in Face)
+                image.Draw(face.rect, new Bgr(255, 255, 255), 10);
+            imgOut = image.Bitmap;
+        }
+
+        public static void ColToHSV(Color col, out double h, out double s, out double v)
+        {
+            double 
+                r = col.R * 255 / 100, 
+                g = col.G * 255 / 100, 
+                b = col.B * 255 / 100,
+                min, max;
+
+            max = Math.Max(r, Math.Max(g, b));
+            min = Math.Min(r, Math.Min(g, b));
+
+            v = (min + max) / 2;
+            h = 0x0;
+
+            if (min == max)
+                h = 0;
+            if (max == r && g >= b)
+                h = 60 * ((g - b) / (max - min)) + 0;
+            if (max == r && g < b)
+                h = 60 * ((g - b) / (max - min)) + 360;
+            if (max == g)
+                h = 60 * ((b - r) / (max - min)) + 120;
+            if (max == b)
+                h = 60 * ((r - g) / (max - min)) + 240;
+
+            s = (max == 0) ? 0 : 1d - (1d * min / max);
+            
+            //if (h >= 180) h = h - 180;
+            //if (h < 180) h = h + 180;
+
+            //s = v * s / v * (s - 1) + 1;
+            //v = v * (s - 1) + 1;
+        }
+
+        public static Color ColFromHSV(double hue, double saturation, double value)
+        {
+            double vmin, vinc, vdec, a;
+            int hi;
+            hi = Convert.ToInt32(hue / 60);
+            vmin = (100 - saturation) * value / 100;
+            a = (value - vmin) * ((hue) / 60);
+            vinc = vmin + a;
+            vdec = value - a;
+            
+            switch (hi)
+            {
+                case 0:
+                    return Color.FromArgb(255, (int)value, (int)vinc, (int)vmin);
+                case 1:
+                    return Color.FromArgb(255, (int)vdec, (int)value, (int)vmin);
+                case 2:
+                    return Color.FromArgb(255, (int)vmin, (int)value, (int)vinc);
+                case 3:
+                    return Color.FromArgb(255, (int)vmin, (int)vdec, (int)value);
+                case 4:
+                    return Color.FromArgb(255, (int)vinc, (int)vmin, (int)value);
+                case 5:
+                    return Color.FromArgb(255, (int)value, (int)vinc, (int)vmin);
+                default:
+                    return Color.FromArgb(255, 255, 255, 255);
+            }
+        }
+    }
+}
