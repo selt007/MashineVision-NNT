a80e0e9ed87de18f73431adc58caf6876a24798a
Alexandr <all1992ex@gmail.com>
2018-09-18 15:32:26 +0400

    Partitioning areas, changing project properties.

--- NNTSearchChar/MainForm.cs
+++ NNTSearchChar/MainForm.cs
@@ -21,10 +21,11 @@ namespace NNTSearchChar
         public MainForm()
         {
             InitializeComponent();
-            textFaceId.SetWatermark("Write user name...");
-            bmap = new Bitmap(picture1.Width, picture1.Height);
             picture1.MouseDown += picture1_MouseDown;
             picture1.Paint += picture1_Paint;
+
+            textFaceId.SetWatermark("Write user name...");
+            bmap = new Bitmap(picture1.Width, picture1.Height);
             g = picture1.CreateGraphics();
 
             picture2.Image = ImageResize.ResizeOrigImg(
@@ -77,61 +78,6 @@ namespace NNTSearchChar
         {
             MessageBox.Show($"This button in stage development! \nWith love <3 \n©Sash", "Sorry",
                         MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-
-        private void picture1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                using (Graphics g = Graphics.FromImage(bmap))
-                {
-                    g.FillEllipse(Brushes.Black, e.X, e.Y, 10, 10);
-                }
-                picture1.Invalidate();
-            }
-        }
-
-        private void picture1_MouseDown(object sender, MouseEventArgs e)
-        {
-            CurrentPoint = e.Location;
-        }
-
-        private void picture1_Paint(object sender, PaintEventArgs e)
-        {
-            e.Graphics.DrawImage(bmap, Point.Empty);
-        }
-
-        private void ImageProperty()
-        {
-            double hue, sat, val;
-            Bitmap bm = new Bitmap(picture2.Image);
-
-            for (var i = 0; i < picture2.Height; i++)
-            {
-                bool err = false;
-                for (var j = 0; j < picture2.Width; j++)
-                {
-                    try
-                    {
-                        ImageProcessing.ColToHSV(bm.GetPixel(i, j), out hue, out sat, out val);
-                        bm.SetPixel(i, j, ImageProcessing.ColFromHSV(hue, sat, val));
-                    }
-                    catch (ArgumentException ex)
-                    {
-                        MessageBox.Show($"Сообщение об ошибке: \n{ex}\n\n" +
-                            $"Один из аргументов имеет значение выходящее из-за диапазона [0-255].", "Error!",
-                            MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        err = true; break;
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show($"Неизвестная ошибка !\n\nСообщение об ошибке: \n{ex}", "Error!",
-                            MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        err = true; break;
-                    }
-                }
-                if (err) break;
-            }
         }
     }
 }
--- NNTSearchChar/Properties/Settings.Designer.cs
+++ NNTSearchChar/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace NNTSearchChar.Properties {
+namespace MashineVisionNNT.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
